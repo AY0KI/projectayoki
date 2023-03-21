3bc7213e596dce65ff0421e5204f283e1df2c6c9
AY0KI <abhinandmidukkan@gmail.com>
2023-03-21 03:52:25 -0700

    srr

--- app1/views.py
+++ app1/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render,HttpResponseRedirect
+from app1.models import item,contactMsg
 # Create your views here.
 def index(request):
-    return render(request,'ayoki.html')
+     a=item.objects.all()
+     print(a)
+     return render(request,'ayoki.html',{"det":a})
